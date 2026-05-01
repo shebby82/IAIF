31b12c7a773096095c9faa44689e14608f30428f
sbiboze <65053333+shebby82@users.noreply.github.com>
2026-05-01 15:14:57 -0400

    Correct conditional and loop syntax in linux-command.sh
    Fix syntax errors in conditional and loop statements.

---
+++ linux-command.sh
@@ -0,0 +1,16 @@
+
+
+a=4
+b=10
+
+if [ $a > $b]
+thenif [ $a > $b 
+then]
+   echo "a is greater than b"
+else
+   echo "b is greate than a"
+fi
+
+for i in {1.100}; do echo $1; done
+
+traop command
