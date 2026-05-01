3820e971c40d2218dfee729d052fea8869338c50
sbiboze <65053333+shebby82@users.noreply.github.com>
2026-05-01 13:02:26 -0400

    Clean up node health check script output
    Removed echo statements for disk space, memory, and CPU.

---
+++ nodehealthcheck2.sh
@@ -0,0 +1,17 @@
+
+##########################################
+# Author: Shabani Biboze
+# Date: 05/01/2026
+#
+# This script outputs the node health
+#
+#Version: v1
+########################################
+set -x # debug mod
+
+df -h
+
+free -g
+
+
+nproc
